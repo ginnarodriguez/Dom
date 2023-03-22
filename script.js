95a96494b0286a2507652cf410ab86d334ea9bd3
ginnarodriguez <grodriguez47@gmail.com>
2023-03-22 16:25:30 -0500

    task

---
+++ script.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector('[data-form-btn]');
+
+const createTask = (evento) => {
+  evento.preventDefault();/*para que no cargue ni se borre lo que se ingresa*/
+  const input = document.querySelector('[data-form-input]');
+  const value = input.value
+  const list =document.querySelector("[data-list]")
+  const task =document.createElement("li");
+  task.classList.add("card");
+  input.value = "";
+  const content = `<div>
+  <i class="far fa-check-square icon"></i>
+  <span class="task">${value}</span>
+</div><i class="fas fa-trash-alt trashIcon icon"></i>`
+task.innerHTML = content;
+
+list.appendChild(task);
+  console.log(content);
+};
+
+console.log(btn);
+
+//Arrow functions o funciones anonimas
+btn.addEventListener('click', createTask);
