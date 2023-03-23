d558bbc57a5b1e31cdc77add5eb0a97d5a4c80c4
ginnarodriguez <grodriguez47@gmail.com>
2023-03-23 16:04:03 -0500

    DOM

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+
+
 const btn = document.querySelector('[data-form-btn]');
+import checkComplete from "./componentes/checkComplete.js";
+import deleteIcon from "./componentes/deleteIcon.js";
 
 const createTask = (evento) => {
   evento.preventDefault();/*para que no cargue ni se borre lo que se ingresa*/
@@ -8,17 +12,28 @@ const createTask = (evento) => {
   const task =document.createElement("li");
   task.classList.add("card");
   input.value = "";
-  const content = `<div>
-  <i class="far fa-check-square icon"></i>
-  <span class="task">${value}</span>
-</div><i class="fas fa-trash-alt trashIcon icon"></i>`
-task.innerHTML = content;
 
+
+  const taskContent = document.createElement("div")
+  
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
+  titleTask.innerText = value;
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
+  
+  
+//task.innerHTML = content;
+task.appendChild(taskContent);
+task.appendChild(deleteIcon());
 list.appendChild(task);
-  console.log(content);
 };
 
-console.log(btn);
 
 //Arrow functions o funciones anonimas
 btn.addEventListener('click', createTask);
+
+
+
+
+
